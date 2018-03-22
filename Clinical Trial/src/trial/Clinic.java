94fb2da79245c88c128d173795bc9463436f4c6d
Adam <Adam@10.169.190.193>
2018-03-22 12:36:57 -0500

    done

--- Clinical Trial/src/trial/Clinic.java
+++ Clinical Trial/src/trial/Clinic.java
@@ -6,40 +6,28 @@ public class Clinic {
 	private String name;
 	private String ID;
 	
-	
-	
 	public Clinic(String name, String id){
-		
-		
+			
 		this.name= name;
 		
 		this.ID= id;		
 	}
 
-
-
 	public String getName() {
 		return name;
 	}
-
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-
 	public String getID() {
 		return ID;
 	}
 
-
-
 	public void setID(String iD) {
 		ID = iD;
 	}
-
 
 
 	@Override
