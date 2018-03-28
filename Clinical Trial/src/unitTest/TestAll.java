c0e9ab874181df50995671ae5b6f67c64aa5442b
Tony Peraza <tony.peraza86@gmail.com>
2018-03-28 14:35:04 -0500

    Changed junit error logging

--- Clinical Trial/src/unitTest/TestAll.java
+++ Clinical Trial/src/unitTest/TestAll.java
@@ -1,20 +1,20 @@
 package unitTest;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class TestAll {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(JunitTestSuite.class);
-
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
-      }
-      boolean successful = result.wasSuccessful();
-      if (successful) {
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(JunitTestSuite.class);
+		boolean successful = result.wasSuccessful();
+		if (successful) {
 			System.out.println("All tests passed.");
-		}else {
-			System.out.println("Test failed.");
+		} else {
+
+			for (Failure failure : result.getFailures()) {
+				System.out.println("FAILURE: " + failure.toString());
+			}
 		}
-   }
-}  	
+	}
+}
