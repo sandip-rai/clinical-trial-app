9aa5e78f1a7a2057cba5fdb61a2bb77debc4b032
xLAZARx <xLAZARx@xLAZARx-PC>
2018-03-27 23:07:12 -0500

    Corrected the test unit for findClinic/patient
      the methods are being called now  asserting the return values as being
    object to the classes Clinic/patient

--- Clinical Trial/src/unitTest/ClincalTrailUnitTest.java
+++ Clinical Trial/src/unitTest/ClincalTrailUnitTest.java
@@ -26,11 +26,23 @@ public class ClincalTrailUnitTest {
 	// unit test method to test the findPatient method 
 	public void uTestForFindPatient(){
 		
-		Patient obj = new Patient("001");
 		
-		Assert.assertNotNull(obj);
+		ClinicalTrial  clt = new ClinicalTrial();
 		
-		Assert.assertTrue(obj instanceof Patient);
+	
+		//adding a single object of Patient
+		clt.addPatient("001");
+		
+		//call to findPatient returning an object if Patient is found or null if not
+		Patient obj1 = clt.findPatient("001");
+		Patient obj2 = clt.findPatient("002");
+		
+		//checking if the method returns the object and null 
+		Assert.assertNotNull(obj1);
+		Assert.assertNull(obj2);
+		//checking if objects are in instances of Patient or not
+		Assert.assertTrue(obj1 instanceof Patient);
+		Assert.assertFalse(obj2 instanceof Patient);
 	
 	}
 	
@@ -44,8 +56,14 @@ public class ClincalTrailUnitTest {
 		String id = "CLN001";
 		String name = "Clinic-A";
 		//object 
+		
+		
 		ClinicalTrial  clt = new ClinicalTrial();
+		Assert.assertNotNull(clt);
+		
 		Clinic clinic = clt.addClinic(name, id);
+		Assert.assertNotNull(clinic);
+		
 		Assert.assertEquals(id, clinic.getId());
 		Assert.assertEquals(name, clinic.getName());
 	}
@@ -55,11 +73,21 @@ public class ClincalTrailUnitTest {
 	// unit test method to test the findClinic method 
 	public void uTestForFindClinic(){
 		
-		Clinic obj = new Clinic("Clinic-A","CLN001");
-		
-		Assert.assertNotNull(obj);
-		
-		Assert.assertTrue(obj instanceof Clinic);
+		ClinicalTrial  clt = new ClinicalTrial();
+
+		//adding a single object of Patient
+		clt.addClinic("Clinic-A", "CLN001");
 	
+		//call to findClinic returning an object if clinic is found or null if not
+		Clinic clinic1 = clt.findClinic("CLN001");
+		Clinic clinic2 = clt.findClinic("CLN002");
+		
+		//checking if the method returns the object and null 
+		Assert.assertNotNull(clinic1);
+		Assert.assertNull(clinic2);
+		
+		//checking if objects are in instances of Clinic or not
+		Assert.assertTrue(clinic1 instanceof Clinic);
+		Assert.assertFalse(clinic2 instanceof Clinic);
 	}
 }
