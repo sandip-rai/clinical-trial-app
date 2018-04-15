dfa80692b22fe9bba880a01b39d71446f7612b54
Sandip Rai <sandip.sampanG@gmail.com>
2018-04-15 18:59:40 -0500

    New Directory with the android version

---
+++ ClinicalTrialAndroidVersion/ClinicalTrial/app/src/main/java/com/sandiprai/clinicaltrial/MainMenuActivity.java
@@ -0,0 +1,46 @@
+package com.sandiprai.clinicaltrial;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+
+
+    /**
+     * Method to run when buttonAddPatient is clicked
+     * Creates and starts an intent to move from MainMenuActivity to AddPatientActivity
+     * @param view
+     */
+    public void onClickAddPatient(View view){
+        //Create the intent
+        Intent intent = new Intent(MainMenuActivity.this,
+                AddPatientActivity.class);
+        startActivity(intent); //Start the intent
+    }
+
+    /**
+     * Method to run when buttonAddPatientInfo is clicked
+     * Creates and starts an intent to move from MainMenuActivity to AddPatientInfoActivity
+     * @param view
+     */
+    public void onClickAddPatientInfo(View view){
+        //Create the intent
+        Intent intent = new Intent(MainMenuActivity.this,
+                AddPatientInfoActivity.class);
+        startActivity(intent); //Start the intent
+    }
+
+
+
+
+
+}
