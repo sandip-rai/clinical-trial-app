044de1326096a6ab5f1cdc0404cedb3c48d8ea5c
LitterBeach <35900848+LitterBeach@users.noreply.github.com>
2018-04-15 21:22:30 -0500

    Added SystemSettingsAvtivity
    need File Import functionlity in order to complete settings funcionlity

--- ClinicalTrialAndroidVersion/ClinicalTrial/app/src/main/java/com/sandiprai/clinicaltrial/MainMenuActivity.java
+++ ClinicalTrialAndroidVersion/ClinicalTrial/app/src/main/java/com/sandiprai/clinicaltrial/MainMenuActivity.java
@@ -39,7 +39,12 @@ public class MainMenuActivity extends AppCompatActivity {
         startActivity(intent); //Start the intent
     }
 
-
+    public void onClickSystemSettings(View view){
+        //Create the intent
+        Intent intent = new Intent(MainMenuActivity.this,
+                SystemSettingsActivity.class);
+        startActivity(intent); //Start the intent
+    }
 
 
 
---
+++ ClinicalTrialAndroidVersion/ClinicalTrial/app/src/main/java/com/sandiprai/clinicaltrial/SystemSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.sandiprai.clinicaltrial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SystemSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_settings);
+    }
+}
